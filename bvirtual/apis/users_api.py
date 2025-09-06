c94d6e52b4a74915eded006a69ad396c37a55201
Zubin Juárez <zubinxxi@gmail.com>
2025-09-05 21:54:29 -0500

    Se actualizó users_api.py

--- bvirtual/apis/users_api.py
+++ bvirtual/apis/users_api.py
@@ -1,9 +1,17 @@
 import reflex as rx 
+from fastapi import FastAPI, Depends
 
 from reflex_local_auth.user import LocalUser
 from bvirtual.models.auth.auth_models import UserInfo, User_Role
 
+# Create a FastAPI app
+fastapi_app = FastAPI(title="My API")
+
 class UsersAPI:
+    """API for user-related operations."""
+    users: list[UserInfo] = []
+
+
     @staticmethod
     def get_user_info_by_user_id(user_id: int) -> UserInfo:
         """Get user info by user ID."""
@@ -24,7 +32,7 @@ class UsersAPI:
         return rx.session.exec(rx.select(UserInfo)).all()
 
 # Aquí es donde cambias el decorador.
-@rx.get("/api/users/")
+@fastapi_app.get("/users/")
 def get_users() -> list[UserInfo]:
     """API endpoint to get all users."""
     return UsersAPI.get_all_users()
