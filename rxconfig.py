42c50fc0fc40b81ee38f3d9eb1decb3085a76989
Zubin Juárez <zubinxxi@gmail.com>
2025-09-05 21:34:58 -0500

    se cambio la ruta a la app en rx.config.py

--- rxconfig.py
+++ rxconfig.py
@@ -3,7 +3,6 @@ import reflex as rx
 config = rx.Config(
     app_name="bvirtual",
     db_url="mysql+mysqldb://bvirtual:1234567.@localhost/bvirtual",
-    api_url="http://localhost:8000",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
