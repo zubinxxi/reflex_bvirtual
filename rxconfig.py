182be74383e653c0e504b68a5bd76bb3e668841d
Zubin Juárez <zubinxxi@gmail.com>
2025-09-12 10:12:51 -0500

    Se agregó configuración de variable de entorno para la conexión a base de datos

--- rxconfig.py
+++ rxconfig.py
@@ -1,8 +1,14 @@
+import os
 import reflex as rx
+from dotenv import load_dotenv
+
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
+DB_CONFIG = os.getenv("DB_CONFIG")
 
 config = rx.Config(
     app_name="bvirtual",
-    db_url="mysql+mysqldb://bvirtual:1234567.@localhost/bvirtual",
+    db_url=DB_CONFIG,
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
