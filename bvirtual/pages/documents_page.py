c22cb88a685cf86baad7184eb937e8ab10825fb0
Zubin Juárez <zubinxxi@gmail.com>
2025-09-12 21:52:42 -0500

    Se agregaron las páginas para el estante y los documentos

---
+++ bvirtual/pages/documents_page.py
@@ -0,0 +1,24 @@
+import reflex as rx 
+import reflex_local_auth
+
+from bvirtual.template import template
+
+@rx.page(route="/documentos", title="Documentos | Biblioteca Virtual")
+@template
+@reflex_local_auth.require_login
+def documents() -> rx.Component:
+    return rx.box(
+        rx.vstack(
+            rx.heading(
+                "Documentos", 
+                size="4", 
+                font_family="Staatliches",
+                letter_spacing="2px",
+            ),
+            width="100%",
+        ),
+        #loading_table_users(),
+        padding_y="20px",
+        width="100%",
+        
+    )
