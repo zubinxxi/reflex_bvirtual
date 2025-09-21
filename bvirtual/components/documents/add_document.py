095ab5aeb7fb83dff68dbdb0d03eec0b18967c14
Zubin Juárez <zubinxxi@gmail.com>
2025-09-21 13:24:59 -0500

    Se actualizó el componente add_document.py para que maneje la subida de documentos todo en el mismo archivo

--- bvirtual/components/documents/add_document.py
+++ bvirtual/components/documents/add_document.py
@@ -1,12 +1,75 @@
 import reflex as rx 
 
 # States
+#from bvirtual.states.state import UploadDocumentState
 from bvirtual.states.documents.add_document_state import AddDocumentState
 
 # Components
-from bvirtual.components.files.upload import upload_document
+#from bvirtual.components.files.upload import upload_document
 
 color = "rgb(107,99,246)"
+
+def upload_document(label: str) -> rx.Component:
+    return rx.vstack(
+        rx.text(label),
+        rx.upload(
+            rx.vstack(
+                rx.button(
+                    "Seleccione el archivo",
+                    color="#424242",
+                    bg="white",
+                    border=f"1px solid {rx.color('cyan')}",
+                ),
+                rx.text(
+                    "Arrastre y suelte archivos aquí o haga clic para seleccionar archivos"
+                ),
+            ),
+            id="upload1",
+            accept={
+                "application/pdf": [".pdf"],
+                #"image/png": [".png"],
+                #"image/jpeg": [".jpg", ".jpeg"],
+                #"image/gif": [".gif"],
+                #"image/webp": [".webp"],
+                #"text/html": [".html", ".htm"],
+            },
+            max_files=1,
+            border=f"1px dotted {rx.color('cyan')}",
+            padding="5em",
+            width="100%",
+        ),
+        rx.hstack(
+            rx.button(
+                "Cargar",
+                on_click=AddDocumentState.handle_upload(
+                    rx.upload_files(upload_id="upload1"),
+                ),
+            ),
+            rx.cond(
+                AddDocumentState.uploading,
+                rx.button(
+                    "Limpiar",
+                    on_click=AddDocumentState.cancel_upload,
+                ),
+            ),
+            
+            rx.cond(
+                AddDocumentState.uploading,
+                rx.hstack(
+                    rx.text(rx.selected_files("upload1")),
+                    rx.icon("circle-check", color="green")
+                ),
+                rx.text(rx.selected_files("upload1")),
+            ),
+
+            justify="start",
+            align="center",
+            width="100%"
+        ),
+        width="100%",
+        justify="center",
+        align="start",
+    ),
 
 def form_add_document():
 
