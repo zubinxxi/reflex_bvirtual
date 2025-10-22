fb5541af105859843ce68bec73528d6356af7408
Zubin Juárez <zubinxxi@gmail.com>
2025-10-22 15:27:39 -0500

    Se agrego el componente que muestra los documentos en la página de inicio

--- bvirtual/pages/index.py
+++ bvirtual/pages/index.py
@@ -1,11 +1,13 @@
 import reflex as rx
 import reflex_local_auth
 
+from bvirtual.components.index.document_section import document_section
 from bvirtual.template_web import template_web
 
 # State
 from bvirtual.states.state import MyLocalAuthState
 from bvirtual.states.state import MenuItemsStates
+from bvirtual.states.state import IndexState
 
 # Components
 from bvirtual.components.index.navbar import navbar_buttons
@@ -18,13 +20,13 @@ from bvirtual.components.index.footer import footer
 @rx.page(
     route="/", 
     title="Inicio | Biblioteca Virtual",
-    on_load=MenuItemsStates.items_menu,
+    on_load=IndexState.load_shelves,
 )
 @template_web
 def index() -> rx.Component:
     return rx.box(
         hero(),
-        menu_category(),
+        document_section(),
         footer(),
         
     )
