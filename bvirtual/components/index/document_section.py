7ecb69fccc4f344d987e0104e7988096f190e15b
Zubin Juárez <zubinxxi@gmail.com>
2025-10-25 14:41:01 -0500

    Se modificó el componente para que se muestren los documentos

--- bvirtual/components/index/document_section.py
+++ bvirtual/components/index/document_section.py
@@ -1,9 +1,17 @@
 import reflex as rx 
 
+
+#models
+from bvirtual.models.documents.document_models import Documents
+
 # State
 from bvirtual.states.state import IndexState
 
+def show_document(documents: Documents) -> rx.Component:
+    pass
+
 def document_section() -> rx.Component:
+    
     return rx.box(
         rx.vstack(
             rx.input(
@@ -40,21 +48,73 @@ def document_section() -> rx.Component:
                     width="100%",
                 ),
 
-                width='10%',
-                margin_x='20px',
+                border_right=f"1px solid {rx.color('cyan')}",
+
+                width='15%',
+                height="80vh",
+                padding_right='20px',
             ),
+
             rx.vstack(
                 rx.text("Documentos: ", size="5", weight='bold', color=rx.color("teal")),
+                rx.grid(
+                    rx.foreach(
+                        IndexState.documents, 
+                        lambda documents: rx.dialog.root(
+                                rx.dialog.trigger(
+                                    rx.button(
+                                        rx.tooltip(
+                                            rx.image(
+                                                src=f"{rx.get_upload_url(documents.name)}"
+                                            ),
+                                            content="Ver PDF",
+                                        ),
+                                        color_scheme="tomato",
+                                        height="100%",
+                                        variant="ghost",
+                                    ),
+                                    
+                                ),
+
+                                rx.dialog.content(
+                                    rx.dialog.title(
+                                        documents.name,
+                                    ),
+
+                                    rx.html(
+                                        f"""
+                                        <iframe 
+                                            src="{rx.get_upload_url(documents.name)}" 
+                                            width="100%" 
+                                            height="800px"
+                                            style="border: 1px solid #e0e0e0; border-radius: 8px;"
+                                        >
+                                        </iframe>
+                                        """,
+                                        width="100%",
+                                    ),
+
+                                    max_width="700px",
+                                    height="900px",
+                                ),
+                            ),
+                    ),
+                    columns="3",
+                    spacing="4",
+                    width="100%",
+                ),
                 width='90%',
                 align='center',
-                border='1px solid #e0e0e0',
+                height="80vh",
             ),
-            #spacing='8',
+
             justify='center',
-            border='1px solid #e0e0e0',
+            #border='1px solid #e0e0e0',
+            padding="20px",
         ),
 
-        width='100%',
+        on_mount=IndexState.list_documents,
+        width='100%',        
         align='center',
         padding="20px",
     )
